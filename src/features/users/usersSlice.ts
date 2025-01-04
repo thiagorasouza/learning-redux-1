9dac187ada5f3d8d27f4336bef5f6b85a8f162df
Thiago Souza <thiagorasouza@gmail.com>
2025-01-04 03:07:21 +0000

    feat: implement users endpoints and selectors

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,39 +1,28 @@
-import { client } from '@/api/client'
 import { RootState } from '@/app/store'
-import { createAppAsyncThunk } from '@/app/withTypes'
+import { apiSlice } from '@/features/api/apiSlice'
 import { selectCurrentUsername } from '@/features/auth/authSlice'
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
+import { createSelector } from '@reduxjs/toolkit'
 
-interface User {
+export interface User {
   id: string
   name: string
 }
 
-export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
-  const response = await client.get<User[]>('/fakeApi/users')
-  return response.data
-})
+export const selectUsersResult = apiSlice.endpoints.getUsers.select()
 
-const usersAdapter = createEntityAdapter<User>()
+export const selectAllUsers = createSelector(selectUsersResult, (userResult) => userResult?.data ?? [])
 
-const initialState = usersAdapter.getInitialState()
-
-const usersSlice = createSlice({
-  name: 'users',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
-  },
-})
-
-export default usersSlice.reducer
-
-export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(
-  (state: RootState) => state.users,
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state: RootState, userId: string) => userId,
+  (users, userId) => users.find((user) => user.id === userId),
 )
 
 export const selectCurrentUser = (state: RootState) => {
   const currentUsername = selectCurrentUsername(state)
   return currentUsername ? selectUserById(state, currentUsername) : undefined
 }
+
+// export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(
+//   (state: RootState) => state.users,
+// )
--- src/main.tsx
+++ src/main.tsx
@@ -9,14 +9,14 @@ import './primitiveui.css'
 import './index.css'
 import { store } from '@/app/store'
 import { Provider } from 'react-redux'
-import { fetchUsers } from '@/features/users/usersSlice'
+import { apiSlice } from '@/features/api/apiSlice'
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
 
-  store.dispatch(fetchUsers())
+  store.dispatch(apiSlice.endpoints.getUsers.initiate())
 
   const root = createRoot(document.getElementById('root')!)
 
